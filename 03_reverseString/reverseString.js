a00b7d6e1a4626b9ddc731235354faf29c4feb77
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 12:26:28 +0300

    Implement reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splittedString = string.split('');
+    splittedString.reverse();
+    return splittedString.join('');
 };
 
 // Do not edit below this line
