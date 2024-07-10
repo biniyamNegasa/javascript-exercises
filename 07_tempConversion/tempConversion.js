fa9bc5daee7256938dd795c5226f20f1ed63cb07
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 13:28:48 +0300

    Test for all cases provided

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(number) {
   const celsius = (number - 32) * (5/9);
-  return Math.round(celsius, 1);
+  return Math.round(celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(number) {
   const fahrenheit = (number * (9/5)) + 32;
-  return Math.round(fahrenheit, 1);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
