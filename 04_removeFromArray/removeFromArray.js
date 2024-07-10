4d3cd71eb279e4cc5015d6f08ad7c987242509e0
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 12:51:27 +0300

    Implement removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...items) {
+    const filteredArray = [];
+
+    for (const element of list) {
+        if (!isFound(items, element)) {
+            filteredArray.push(element);
+        }
+    }
+
+    return filteredArray;
+
 };
+
+function isFound(items, element) {
+    for (const item of items) {
+        if (item == element) return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
