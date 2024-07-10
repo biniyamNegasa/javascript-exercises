69872165114acef236e0634945d906f70d157d42
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 13:02:07 +0300

    Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != 'number' || typeof end != 'number' ||
+        start < 1 || end < 1
+    ) {
+        return 'ERROR';
+    }
+    if (start > end ) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+    start -= 1;
+    // sum formula
+    return (end*(end+1))/2 - (start*(start+1))/2;
 };
 
 // Do not edit below this line
