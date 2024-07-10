c975953bc82130535b550a726fe98f3efdbd7613
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 13:24:16 +0300

    Implement convertToCelsius and converToFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  const celsius = (number - 32) * (5/9);
+  return Math.round(celsius, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  const fahrenheit = (number * (9/5)) + 32;
+  return Math.round(fahrenheit, 1);
 };
 
 // Do not edit below this line
