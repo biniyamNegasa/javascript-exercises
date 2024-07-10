d80d06ec60421ef54dff6a5e6ffdca1b7369b30b
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-10 12:15:22 +0300

    Implement repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let currentString = '';
+    for (let i = 0; i < number; i++) {
+        currentString += string;
+    }
 
+    return currentString;
 };
 
 // Do not edit below this line
