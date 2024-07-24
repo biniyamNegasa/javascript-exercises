03d3eac29d251894146e030f03d164501d6ac50b
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-24 14:59:18 +0300

    Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 0) return "OOPS";
+    if (num < 2) return num;
+    a = 0;
+    b = 1;
+    for (let i = 0; i < num-1; i++){
+        temp = a;
+        a = b;
+        b += temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
