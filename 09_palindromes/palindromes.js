04681b0e89b4c0b8bd2e8c77147ca875ce40364b
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-24 14:52:11 +0300

    Implement palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let list = [];
+    const A = 'A'.charCodeAt(0);
+    const Z = 'Z'.charCodeAt(0);
+    const a = 'a'.charCodeAt(0);
+    const z = 'z'.charCodeAt(0);
+    const zero = '0'.charCodeAt(0);
+    const nine = '9'.charCodeAt(0);
 
+    for (let ch of string){
+        let val = ch.charCodeAt(0);
+
+        if (val <= nine && val >= zero){
+            list.push(ch);
+        }
+        else if (val <= Z && val >= A){
+            list.push(ch.toLowerCase());
+        }
+        else if (val <= z && val >= a){
+            list.push(ch);
+        }
+    }
+    n = list.length;
+    i = 0;
+    j = n - 1;
+    while (i < j){
+        if (list[i] !== list[j]) return false;
+        i += 1
+        j -= 1
+    }
+    return true;
 };
 
 // Do not edit below this line
