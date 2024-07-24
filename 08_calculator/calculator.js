70b61d817a563de8b0ea0070b187f004a9debb5f
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-24 14:31:50 +0300

    Implement calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	let curr = 0;
+  for (const num of list) {
+    curr += num;
+  }
+  return curr;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let curr = 1;
+  for (let num of list){
+    curr *= num;
+  }
+  return curr;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let start = 1;
+  for (let i = 0; i < exp; i++){
+    start *= num;
+  }
+  return start;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1) return 1;
+  let start = 1;
+  for (let i = num; i > 1; i--){
+    start *= i;
+  }
+
+  return start;
 };
 
 // Do not edit below this line
